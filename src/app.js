027a5015eeae104cb828193dd7080b94667ce69e
pankaj <pk8880986@gmail.com>
2024-05-01 21:27:40 +0530

    error handlig

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-import express from 'express'
+import express, { json } from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN ,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 
 export {app};
